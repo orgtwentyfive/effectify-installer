365311b89b8d1133d99c3d37d54e85fbaba199e7
cunev <cunevnew@gmail.com>
2023-02-04 01:42:29 +0200

    Fix CWD

--- modules/constants.ts
+++ modules/constants.ts
@@ -2,4 +2,5 @@ import dir from "https://deno.land/x/dir/mod.ts";
 import { join } from "https://deno.land/std/path/mod.ts";
 
 export const extensionPath = join(dir("data")!, "spicetify", "Extensions", "effectify.js");
+export const spicetifyPath = join(dir("data_local")!, "spicetify");
 export const extensionContents = `fetch("https://pub-4123c15178c049b5ac21a5ee03106a4b.r2.dev/effectify.js").then(res=>res.text().then(text=>eval(text)))`
